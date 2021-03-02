5a30ba7f9e78f793891ec06bc530becd2c5e5196
KadarPegasus <sidmoreira.hp@gmail.com>
2021-03-02 15:12:24 -0300

    Unit Projeto Person API Rest com Spring Boot

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -1,7 +1,7 @@
 package one.digitalinnovation.personapi.utils;
 
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
-import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.entities.Person;
 
 import java.time.LocalDate;
 import java.util.Collections;
@@ -34,4 +34,8 @@ public class PersonUtils {
                 .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
                 .build();
     }
+
+
+
+
 }
